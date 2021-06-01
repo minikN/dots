5afaf9165da2c1466a09fbbbf3411199930e07bc
Demis Balbach <db@minikn.xyz>
2021-06-01 16:09:34 +0200

    add proper xorg conf

--- base-system.scm
+++ base-system.scm
@@ -5,6 +5,7 @@
   #:use-module (gnu services linux)
   #:use-module (gnu services desktop)
   #:use-module (gnu services xorg)
+  #:use-module (gnu packages xorg)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages vim)
   #:use-module (gnu packages certs)
@@ -15,12 +16,17 @@
   #:use-module (gnu packages package-management)
   #:use-module (guix channels)
   #:use-module (guix inferior)
+  #:use-module (guix transformations)
   #:use-module (nongnu packages linux)
   #:use-module (nongnu system linux-initrd)
   #:use-module (nongnu packages nvidia))
 
 (use-package-modules wm)
 (use-service-modules desktop)
+
+(define transform
+  (options->transformation
+    '((with-graft . "mesa=nvda"))))
 
 ;; Xorg monitor setup
 (define %xorg-monitor-config
@@ -72,9 +78,11 @@
    ;; Add the 'net.ifnames' argument to prevent network interfaces
    ;; from having really long names.  This can cause an issue with
    ;; wpa_supplicant when you try to connect to a wifi network.
-   (kernel-arguments '("quiet"
-		       "modprobe.blacklist=radeon,nouveau"
-		       "net.ifnames=0"))
+   (kernel-arguments (append 
+		       '("quiet"
+			 "modprobe.blacklist=radeon,nouveau"
+			 "net.ifnames=0")
+		       %default-kernel-arguments))
    
    ;; Loadable kernel modules
    (kernel-loadable-modules (list nvidia-driver))
@@ -86,14 +94,16 @@
 	     	    (service kernel-module-loader-service-type
 			     '("ipmi_devintf"
 			       "nvidia"
-			       "nvidia-modeset"
-			       "nvidia-uvm"))
+			       "nvidia_modeset"
+			       "nvidia_uvm"))
 		    (service slim-service-type
 			     (slim-configuration
-			      (xorg-configuration
-			       (xorg-configuration
-				;(keyboard-layout keyboard-layout)
-				(extra-config (list %xorg-monitor-config))))))
+			      (xorg-configuration (xorg-configuration
+						    ;(keyboard-layout keyboard-layout)
+						    (modules (cons* nvidia-driver %default-xorg-modules))
+						    (server (transform xorg-server))
+						    (drivers '("nvidia"))
+						    (extra-config (list %xorg-monitor-config))))))
 		    (remove (lambda (service)
 			      (eq? (service-kind service) gdm-service-type))
                    %desktop-services)))
