85bcbb9c4f1d3a8f91a790443d543aaff6a91f1c
Demis Balbach <db@minikn.xyz>
2021-06-23 19:25:04 +0200

    tidy file up

--- base-system.scm
+++ base-system.scm
@@ -24,10 +24,31 @@
   #:use-module (nongnu packages linux)
   #:use-module (nongnu system linux-initrd))
 
+(define %guix-channels
+  (scheme-file
+   "channels.scm"
+   #~(cons* (channel
+             (name 'nonguix)
+             (url "https://gitlab.com/nonguix/nonguix")
+             (introduction
+              (make-channel-introduction
+               "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+               (openpgp-fingerprint
+                "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+            (channel
+             (name 'flat)
+             (url "https://github.com/flatwhatson/guix-channel.git")
+             (introduction
+              (make-channel-introduction
+               "33f86a4b48205c0dc19d7c036c85393f0766f806"
+               (openpgp-fingerprint
+		"736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+            %default-channels)))
+
 (define %sddm-hidpi
   (plain-file
-    "hidpi.conf"
-    "[Wayland]
+   "hidpi.conf"
+   "[Wayland]
     EnableHiDPI=true"))
 
 (define-public base-operating-system
@@ -38,17 +59,17 @@
 
    ;; Kernel arguments
    (kernel-arguments (append 
-		       '("quiet"
-			 "modprobe.blacklist=nouveau"
-			 "net.ifnames=0")
-		       %default-kernel-arguments))
- 
+		      '("quiet"
+			"modprobe.blacklist=nouveau"
+			"net.ifnames=0")
+		      %default-kernel-arguments))
+   
    ;; Machine settings
    (host-name "geekcave")
    (timezone "Europe/Berlin")
    (locale "en_US.utf8")
    (keyboard-layout (keyboard-layout "us" "altgr-intl"))
-   		
+   
    ;; User account
    (users (cons (user-account
 		 (name "db")
@@ -63,29 +84,31 @@
 		%base-user-accounts))
 
    ;; Services
-   (services (cons* (extra-special-file "/etc/sddm.conf.d/hidpi.conf" %sddm-hidpi)
-		    (service sddm-service-type)
-	       (remove (lambda (service)
-			      (eq? (service-kind service) gdm-service-type))
-			    (modify-services %desktop-services
-					     (guix-service-type
-					      config =>
-					      (guix-configuration
-					       (inherit config)
-					       (substitute-urls
-						(append (list "https://mirror.brielmaier.net")
-							%default-substitute-urls))
-					       (authorized-keys
-						(append (list (local-file "/home/db/.config/guix/mirror.brielmaier.net.pub"))
-							%default-authorized-guix-keys))))))))
+   (services (cons* 
+	      (extra-special-file "/etc/guix/channels.scm" %guix-channels)
+	      (extra-special-file "/etc/sddm.conf.d/hidpi.conf" %sddm-hidpi)
+	      (service sddm-service-type)
+	      (remove (lambda (service)
+			(eq? (service-kind service) gdm-service-type))
+		      (modify-services %desktop-services
+				       (guix-service-type
+					config =>
+					(guix-configuration
+					 (inherit config)
+					 (substitute-urls
+					  (append (list "https://mirror.brielmaier.net")
+						  %default-substitute-urls))
+					 (authorized-keys
+					  (append (list (local-file "/home/db/.config/guix/mirror.brielmaier.net.pub"))
+						  %default-authorized-guix-keys))))))))
 
 
-  ;; Bootloader		
+   ;; Bootloader		
    (bootloader (bootloader-configuration
 		(bootloader grub-efi-bootloader)
 		(target "/boot/efi")
 		(timeout 3)))
-		
+   
    ;; File systems
    (file-systems (cons* (file-system ;; System partition
 			 (device (file-system-label "GUIX"))
@@ -95,29 +118,29 @@
 			 (device (file-system-label "BOOT"))
 			 (mount-point "/boot/efi")
 			 (type "vfat"))
-		       %base-file-systems))
+			%base-file-systems))
 
    ;; Packages to install
    (packages
-     (append
-       (list git
-	     curl
-	     vim
-	     nss-certs
-	     sway
-	     swaybg
-	     swayidle
-	     swaylock
-	     mesa
-	     dxvk
-	     vkd3d
-	     mesa-headers
-	     spirv-cross
-	     spirv-tools
-	     mesa-utils
-	     spirv-headers
-	     zsh)
-       %base-packages))
+    (append
+     (list git
+	   curl
+	   vim
+	   nss-certs
+	   sway
+	   swaybg
+	   swayidle
+	   swaylock
+	   mesa
+	   dxvk
+	   vkd3d
+	   mesa-headers
+	   spirv-cross
+	   spirv-tools
+	   mesa-utils
+	   spirv-headers
+	   zsh)
+     %base-packages))
 
    (name-service-switch %mdns-host-lookup-nss)))
 base-operating-system
