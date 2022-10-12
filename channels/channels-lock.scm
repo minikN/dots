384dc9e3dade47288769e6674abbc2f011776411
Demis Balbach <db@minikn.xyz>
2022-10-12 20:46:19 +0200

    channels: Update.

--- channels/channels-lock.scm
+++ channels/channels-lock.scm
@@ -3,7 +3,7 @@
         (url "https://github.com/babariviere/guix-emacs")
         (branch "master")
         (commit
-          "c975c5832fc75d2ce6ea6b0b61962a86cf4a5ef4")
+          "bb4152432328ae9ffddf6e69ae023653682dc3fd")
         (introduction
           (make-channel-introduction
             "72ca4ef5b572fea10a4589c37264fa35d4564783"
@@ -25,7 +25,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "e0951349603581895e0ba61f0e7410368ea1902a")
+          "12024f8855983bba21f4f11c33faf24c73f4d855")
         (introduction
           (make-channel-introduction
             "46c1d8bcca674d3a71cd077c52dde9552a89873d"
@@ -36,7 +36,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "3ba8c2b5076cff874f12e2880632ec5e5507acff")
+          "86ec52f66735b122b9035eba56516fd16f3be958")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
