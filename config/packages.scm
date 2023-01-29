aa70dd53d408801cea6d15b0044be98bcd763d1e
Demis Balbach <db@minikn.xyz>
2023-01-29 18:05:57 +0100

    config: packages: Remove obsolete packages.

--- config/packages.scm
+++ config/packages.scm
@@ -1,48 +1,30 @@
 (define-module (config packages)
-  #:use-module (gnu build chromium-extension)
-  #:use-module (gnu packages)
-  #:use-module (gnu packages autotools)
-  #:use-module (gnu packages cmake)
-  #:use-module (gnu packages curl)
-  #:use-module (gnu packages emacs-xyz)
-  #:use-module (gnu packages freedesktop)
-  #:use-module (gnu packages gl)
-  #:use-module (gnu packages gnome)
-  #:use-module (gnu packages linux)
-  #:use-module (gnu packages sdl)
-  #:use-module (gnu packages stb)
-  #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages web)
-  #:use-module (gnu packages wm)
-  #:use-module (gnu packages video)
-  #:use-module (gnu packages vulkan)
-  #:use-module (gnu packages xorg)
-  #:use-module (gnu packages xdisorg)
   #:use-module (guix gexp)
   #:use-module (guix download)
   #:use-module (guix git-download)
-  #:use-module (guix build-system emacs)
   #:use-module (guix build-system copy)
   #:use-module (guix build-system gnu)
-  #:use-module (guix build-system meson)
   #:use-module (guix packages)
 
-  #:use-module (gnu packages base)
-  #:use-module (gnu packages compression)
-  #:use-module (gnu packages cups)
-  #:use-module (gnu packages fontutils)
-  #:use-module (gnu packages gcc)
-  #:use-module (gnu packages gtk)
-  #:use-module (gnu packages gl)
-  #:use-module (gnu packages glib)
-  #:use-module (gnu packages linux)
-  #:use-module (gnu packages nss)
-  #:use-module (gnu packages xdisorg)
-  #:use-module ((gnu packages xml) :prefix xml:)
-  #:use-module (gnu packages xorg)
-  #:use-module (nonguix build-system binary)
-  #:use-module (guix licenses)
+  #:use-module (gnu build chromium-extension)
 
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages pulseaudio)
+  #:use-module (gnu packages sdl)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages web)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages xorg)
+
+  #:use-module (nongnu packages steam-client)
+
+  #:use-module (guix licenses)
   #:use-module ((guix licenses) #:prefix license:))
 
 (define-public steamos-modeswitch-inhibitor
@@ -75,7 +57,8 @@
    (license license:gpl3+)))
 
 (define-public steamos-compositor-plus
-  (let ((commit "7c0011cf91e87c30c2a630fee915ead58ef9dcf5")
+  (let ((commit "e4b99dd5f56a388aa24fe3055d0e983cb3d5d32a")
+        ;;(commit "7c0011cf91e87c30c2a630fee915ead58ef9dcf5")
         (revision "0")
         (version "1.3.0"))
     (package
@@ -88,14 +71,16 @@
              (url "https://github.com/ChimeraOS/steamos-compositor-plus")
              (commit commit)))
        (sha256
-        (base32 "1bjl517bw10w18f4amdz3kwzkdz8w6wg8md2bk3wpqjrn26p45gd"))
+        (base32 "0rqckj05389djc4ahzfy98p3z0p884gbbl94lbm06za72bhnidr5")
+       ;;(base32 "1bjl517bw10w18f4amdz3kwzkdz8w6wg8md2bk3wpqjrn26p45gd")
+       )
        (file-name (git-file-name name version))))
      (arguments
       (list
        #:phases
        #~(modify-phases
           %standard-phases
-          (add-after 'unpack 'copy-scripts
+          (add-after 'patch-source-shebangs 'copy-scripts
                      (lambda* (#:key outputs #:allow-other-keys)
                        (let* ((out (assoc-ref outputs "out"))
                               (bin (string-append out "/bin"))
@@ -111,17 +96,23 @@
                      (lambda* (#:key inputs outputs #:allow-other-keys)
                        (let* ((out (assoc-ref outputs "out"))
                               (share (string-append out "/share"))
+                              (src (string-append share "/steamos-compositor-plus/bin"))
                               (bin (string-append out "/bin"))
+                              (aplay (string-append (assoc-ref inputs "alsa-utils") "/bin/aplay"))
+                              (pamixer (string-append (assoc-ref inputs "pamixer") "/bin/pamixer"))
+                              (udevadm (string-append (assoc-ref inputs "eudev") "/bin/udevadm"))
                               (xset (string-append (assoc-ref inputs "xset") "/bin/xset"))
+                              (xrandr (string-append (assoc-ref inputs "xrandr") "/bin/xrandr"))
+                              (xinput (string-append (assoc-ref inputs "xinput") "/bin/xinput"))
+                              (grep (string-append (assoc-ref inputs "grep") "/bin/grep"))
+                              (steam (string-append (assoc-ref inputs "steam") "/bin/steam"))
                               (modeswitch-inhibitor (string-append
                                                      (assoc-ref inputs "steamos-modeswitch-inhibitor")
                                                      "/lib/libmodeswitch_inhibitor.so")))
-                         ;; We could replace STEAMCMD="steam ..." with a steam package we add as an
-                         ;; input. But we want the package to use the already installed steam pkg,
-                         ;; don't we?
                          (substitute* "./usr/bin/steamos-session"
                                       ;; No need to export scripts to PATH.
                                       (("export.+\\{PATH\\}" line) (string-append "#" line))
+                                      (("\"steam ") (string-append "\"" steam " "))
                                       (("export.+so" line) (string-append "export" " " "LD_PRELOAD=" modeswitch-inhibitor))
                                       (("set_hd_mode.sh") (string-append bin "/scripts/set_hd_mode.sh"))
                                       (("loadargb_cursor") (string-append bin "/loadargb_cursor"))
@@ -131,7 +122,34 @@
                                       (("/usr/share/pixmaps/steam-bootstrapper.jpg")
                                        (string-append share "/pixmaps/steam-bootstrapper.jpg"))
                                       (("steamcompmgr") (string-append bin "/steamcompmgr"))
-                                      (("xset") xset)))))
+                                      (("steam-browser.desktop") (string-append share "/applications/steam-browser.desktop"))
+                                      (("xset") xset))
+                         (substitute* (string-append src "/set_hd_mode.sh")
+                                      (("xrandr") xrandr)
+                                      (("egrep") "grep -E")
+                                      (("grep") grep)
+                                      (("xinput") xinput)
+                                      (("udevadm") udevadm))
+                         (substitute* (string-append src "/screen_toggle.sh")
+                                      (("xrandr") xrandr))
+                         (substitute* (string-append src "/brightness_up.sh")
+                                      (("xrandr") xrandr))
+                         (substitute* (string-append src "/brightness_down.sh")
+                                      (("xrandr") xrandr))
+                         (substitute* (string-append src "/audio_volup.sh")
+                                      (("aplay") aplay)
+                                      (("pamixer") pamixer))
+                         (substitute* (string-append src "/audio_voldown.sh")
+                                      (("aplay") aplay)
+                                      (("pamixer") pamixer))
+                         (substitute* (string-append src "/audio_mute.sh")
+                                      (("aplay") aplay)
+                                      (("pamixer") pamixer))
+                         (substitute* (string-append share "/xsessions/steamos.desktop")
+                                      (("steamos-session") (string-append bin "/steamos-session")))
+                         (substitute* (string-append share "/applications/steam-browser.desktop")
+                                      (("steam ") (string-append steam " ")))
+                         )))
           (add-after 'patch-bin 'copy-bin
                      (lambda* (#:key outputs #:allow-other-keys)
                        (let* ((out (assoc-ref outputs "out"))
@@ -146,14 +164,21 @@
       (list xinit
             xorg-server))
      (inputs
-      (list libxxf86vm
+      (list alsa-utils
+            eudev
+            grep
+            libxxf86vm
             libx11
             libxrender
             libxcomposite
             libgudev
             glu
+            pamixer
             sdl-image
+            steam
+            xinput
             xset
+            xrandr
             steamos-modeswitch-inhibitor))
      (build-system gnu-build-system)
      (home-page "https://github.com/ChimeraOS/steamos-compositor-plus")
@@ -195,178 +220,57 @@ and probably others.")
 ;; Type inference is quite primitive.")
 ;;      (license license:gpl3+))))
 
-
-;; (define-public gamescope
-;;   ;; (let ((commit "3122d1aabec4d8f7df79edb1b76e136c0dd5ffbf")
-;;   (let ((commit "ae7fcc5ba75abe3aed700407c8ce6e2496327029")
+;; (define-public tree-sitter-javascript
+;;   (let ((commit "7a29d06274b7cf87d643212a433d970b73969016")
 ;;         (revision "0")
-;;         (version "3.11.33-jupiter-3.3-1"))
+;;         (version "0.20.0"))
 ;;     (package
-;;      (name "gamescope")
+;;      (name "tree-sitter-javascript")
 ;;      (version (git-version version revision commit))
 ;;      (source
 ;;       (origin
 ;;        (method git-fetch)
 ;;        (uri (git-reference
-;;              (url "https://github.com/Plagman/gamescope")
-;;              (commit commit)
-;;              (recursive? #t)))
+;;              (url "https://github.com/tree-sitter/tree-sitter-javascript")
+;;              (commit commit)))
 ;;        (sha256
-;;         (base32 "19ri8s0x6nxyi77qx05grl08f05pxz1wzjvnkn5fix9ic9fhsb4w"))
+;;         (base32 "1pk6d9g6a7bzhxmwnvfiycarcgz76wq2rgfqr0xjh7y7swfw5hvw"))
 ;;        (file-name (git-file-name name version))))
-;;      (build-system meson-build-system)
+;;      (build-system gnu-build-system)
+;;      (native-inputs
+;;       (list gcc))
 ;;      (arguments
-;;       `(#:phases
+;;       `(#:tests?
+;;         #f
+;;         #:phases
 ;;         (modify-phases
 ;;          %standard-phases
-;;          (add-after 'unpack 'patch-deps
+;;          (delete 'configure)
+;;          (delete 'install)
+;;          (delete 'build)
+;;          (add-after 'unpack 'create-dirs
+;;                     (lambda _ (mkdir "lib")))
+;;          (add-after 'create-dirs 'compile
 ;;                     (lambda* (#:key inputs outputs #:allow-other-keys)
 ;;                       (let* ((out (assoc-ref outputs "out"))
-;;                              (stb-image (string-append (assoc-ref inputs "stb-image") "/include")))
-;;                         (substitute* "./meson.build"
-;;                                      (("stb_dep = dependency\\('stb'\\)") "")
-;;                                      ((", stb_dep,") ""))
-;;                         (invoke "cat" "-n" "./meson.build")))))))
-;;      (native-inputs
-;;       (list cmake
-;;             pkg-config
-;;             stb-image
-;;             glslang))
-;;      (inputs
-;;       (list libx11
-;;             libxdamage
-;;             libxcomposite
-;;             libxrender
-;;             libxext
-;;             libxxf86vm
-;;             libxtst
-;;             libxres
-;;             libxkbcommon
-;;             libcap
-;;             libdrm
-;;             sdl2
-;;             stb-image
-;;             pipewire-0.3
-;;             vulkan-loader
-;;             wayland
-;;             wayland-protocols
-;;             wlroots-0.14.0))
-;;      (home-page "https://github.com/isamert/jsdoc.el")
-;;      (synopsis "Gamescope: The micro-compositor formerly known as steamcompmgr.")
-;;      (description "")
-;;      (license license:gpl3+))))
-
-(define-public tree-sitter-javascript
-  (let ((commit "7a29d06274b7cf87d643212a433d970b73969016")
-        (revision "0")
-        (version "0.20.0"))
-    (package
-     (name "tree-sitter-javascript")
-     (version (git-version version revision commit))
-     (source
-      (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/tree-sitter/tree-sitter-javascript")
-             (commit commit)))
-       (sha256
-        (base32 "1pk6d9g6a7bzhxmwnvfiycarcgz76wq2rgfqr0xjh7y7swfw5hvw"))
-       (file-name (git-file-name name version))))
-     (build-system gnu-build-system)
-     (native-inputs
-      (list gcc))
-     (arguments
-      `(#:tests?
-        #f
-        #:phases
-        (modify-phases
-         %standard-phases
-         (delete 'configure)
-         (delete 'install)
-         (delete 'build)
-         (add-after 'unpack 'create-dirs
-                    (lambda _ (mkdir "lib")))
-         (add-after 'create-dirs 'compile
-                    (lambda* (#:key inputs outputs #:allow-other-keys)
-                      (let* ((out (assoc-ref outputs "out"))
-                             (gcc (string-append (assoc-ref inputs "gcc") "/bin")))
-                        (copy-file "grammar.js" "src/grammar.js")
-                        (with-directory-excursion
-                         (string-append "./src")
-                         (invoke (string-append gcc "/gcc") "-fPIC" "-c" "-I." "parser.c")
-                         (invoke (string-append gcc "/gcc") "-fPIC" "-c" "-I." "scanner.c")
-                         (invoke (string-append gcc "/gcc")
-                                 "-fPIC" "-shared" "parser.o" "scanner.o" "-o"
-                                 "libtree-sitter-javascript.so")))))
-         (add-after 'compile 'symlink
-                    (lambda* (#:key inputs outputs #:allow-other-keys)
-                      (let* ((out (assoc-ref outputs "out"))
-                             (lib (string-append out "/lib")))
-                        (install-file "src/libtree-sitter-javascript.so" lib)))))))
-      (home-page "https://github.com/tree-sitter/tree-sitter-javascript")
-     (synopsis "JavaScript and JSX grammar for tree-sitter.")
-     (description "JavaScript and JSX grammar for tree-sitter.")
-     (license license:expat))))
-
-(define-public rofi-streamlink
-  (let ((commit "e9edeff07b46448eb5bc3c63630261831df4f056")
-        (revision "0")
-        (version "0.1"))
-    (package
-     (name "rofi-streamlink")
-     (version (git-version version revision commit))
-     (source
-      (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/minikN/rofi-streamlink")
-             (commit commit)))
-       (sha256
-        (base32 "0w0hr0w5fngwbxs2zclqgn891lbyzqpfwlvs5lrgs3p6rdr54cvr"))
-       (file-name (git-file-name name version))))
-     (build-system gnu-build-system)
-     (inputs (list
-              curl
-              jq
-              rofi
-              youtube-dl
-              streamlink
-              mpv))
-     (arguments
-      `(#:tests?
-        #f
-        #:phases
-        (modify-phases
-         %standard-phases
-         (delete 'configure)
-         (delete 'install)
-         (delete 'build)
-         (add-after 'unpack 'patch-bin
-                    (lambda* (#:key inputs outputs #:allow-other-keys)
-                      (let* ((out (assoc-ref outputs "out"))
-                             (curl (string-append (assoc-ref inputs "curl") "/bin/curl"))
-                             (jq (string-append (assoc-ref inputs "jq") "/bin/jq"))
-                             (rofi (string-append (assoc-ref inputs "rofi") "/bin/rofi"))
-                             (youtube-dl (string-append (assoc-ref inputs "youtube-dl") "/bin/youtube-dl"))
-                             (streamlink (string-append (assoc-ref inputs "streamlink") "/bin/streamlink"))
-                             (mpv (string-append (assoc-ref inputs "mpv") "/bin/mpv")))
-                        (substitute* "./rofi-ttv"
-                                     (("curl") curl)
-                                     (("jq") jq)
-                                     (("rofi ") (string-append rofi " "))
-                                     (("youtube-dl") youtube-dl)
-                                     (("streamlink") streamlink)
-                                     (("mpv") mpv)))))
-         (add-after 'patch-bin 'copy-bin
-                    (lambda* (#:key outputs #:allow-other-keys)
-                      (let* ((out (assoc-ref outputs "out"))
-                             (bin (string-append out "/bin")))
-                        (install-file "./rofi-ttv" bin)
-                        (chmod (string-append bin "/rofi-ttv") #o555)))))))
-     (home-page "https://github.com/galeo/corfu-doc")
-     (synopsis "A scripts that uses rofi, youtube-dl and mpv to view twitch streams.")
-     (description "A scripts that uses rofi, youtube-dl and mpv to view twitch streams.")
-     (license license:expat))))
+;;                              (gcc (string-append (assoc-ref inputs "gcc") "/bin")))
+;;                         (copy-file "grammar.js" "src/grammar.js")
+;;                         (with-directory-excursion
+;;                          (string-append "./src")
+;;                          (invoke (string-append gcc "/gcc") "-fPIC" "-c" "-I." "parser.c")
+;;                          (invoke (string-append gcc "/gcc") "-fPIC" "-c" "-I." "scanner.c")
+;;                          (invoke (string-append gcc "/gcc")
+;;                                  "-fPIC" "-shared" "parser.o" "scanner.o" "-o"
+;;                                  "libtree-sitter-javascript.so")))))
+;;          (add-after 'compile 'symlink
+;;                     (lambda* (#:key inputs outputs #:allow-other-keys)
+;;                       (let* ((out (assoc-ref outputs "out"))
+;;                              (lib (string-append out "/lib")))
+;;                         (install-file "src/libtree-sitter-javascript.so" lib)))))))
+;;       (home-page "https://github.com/tree-sitter/tree-sitter-javascript")
+;;      (synopsis "JavaScript and JSX grammar for tree-sitter.")
+;;      (description "JavaScript and JSX grammar for tree-sitter.")
+;;      (license license:expat))))
 
 (define-public rofi-ttv
   (let ((commit "e9c722481b740196165f840771b3ae58b7291694")
@@ -459,3 +363,6 @@ them on the badge.
 
 (define-public chromium-web-store/chromium
   (make-chromium-extension chromium-web-store "chromium"))
+
+
+rofi-ttv
