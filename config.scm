b650d54cc5a9ff6b26dabefee327d78a1abfae25
Demis Balbach <db@minikn.xyz>
2023-02-11 09:30:21 +0100

    config: Add elftower machine.

--- config.scm
+++ config.scm
@@ -1,6 +1,7 @@
 (define-module (config)
   #:use-module (config workhorse)
   #:use-module (config geekcave)
+  #:use-module (config elftower)
   #:use-module (ice-9 match)
   #:use-module (rde features))
 
@@ -16,11 +17,19 @@
 (define workhorse-he
   (rde-config-home-environment workhorse-config))
 
+(define elftower-os
+  (rde-config-operating-system elftower-config))
+
+(define elftower-he
+  (rde-config-home-environment elftower-config))
+
 (define (dispatcher)
   (let ((target (getenv "TARGET")))
     (match target
       ("workhorse-he" workhorse-he)
       ("workhorse-os" workhorse-os)
+      ("elftower-he" elftower-he)
+      ("elftower-os" elftower-os)
       ("geekcave-he" geekcave-he)
       ("geekcave-os" geekcave-os))))
 
