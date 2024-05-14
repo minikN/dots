ab958a20e0e5b27fe7ff20ec98076b803fcaac31
Demis Balbach <db@minikn.xyz>
2024-05-15 00:47:10 +0200

    channels: Set up local channels properly

--- channels/channels-local.scm
+++ channels/channels-local.scm
@@ -2,19 +2,29 @@
              (guix channels))
 
 (list
- (channel
-  (name 'guix)
-  (url "file:///home/bob/work/gnu/guix")
-  (introduction
-   (make-channel-introduction
-    "9edb3f66fd807b096b48283debdcddccfea34bad"
-    (openpgp-fingerprint
-     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+ %default-guix-channel
  (channel
   (name 'rde)
-  (url "https://git.sr.ht/~abcdw/rde")
+  (url "file:///home/db/.local/share/git/rde")
+  ;; (introduction
+  ;;  (make-channel-introduction
+  ;;   "257cebd587b66e4d865b3537a9a88cccd7107c95"
+  ;;   (openpgp-fingerprint
+  ;;    "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))
+  )
+ (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix")
   (introduction
    (make-channel-introduction
-    "257cebd587b66e4d865b3537a9a88cccd7107c95"
+    "46c1d8bcca674d3a71cd077c52dde9552a89873d"
     (openpgp-fingerprint
-     "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))))
+     "8141 6036 E81A 5CF7 8F80  1071 ECFC 8398 8B4E 4B9F"))))
+(channel
+  (name 'emacs)
+  (url "https://github.com/babariviere/guix-emacs")
+  (introduction
+   (make-channel-introduction
+    "72ca4ef5b572fea10a4589c37264fa35d4564783"
+    (openpgp-fingerprint
+     "261C A284 3452 FB01 F6DF  6CF4 F9B7 864F 2AB4 6F18")))))
--- channels/channels-lock-local.scm
+++ channels/channels-lock-local.scm
@@ -1,24 +1,41 @@
 (use-modules (guix channels))
 
 (list (channel
-        (name 'rde)
-        (url "https://git.sr.ht/~abcdw/rde")
-        (branch "master")
-        (commit
-          "e4fe6ef24219b3b2a512ae96ef61f2bbad44dc04")
-        (introduction
-          (make-channel-introduction
-            "257cebd587b66e4d865b3537a9a88cccd7107c95"
-            (openpgp-fingerprint
-              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
-      (channel
         (name 'guix)
-        (url "file:///home/bob/work/gnu/guix")
+        (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "e6acde4a95dea5b9a27416e58c6f904ff1e9eba6")
+          "898b5f30f3d485d48275c920da172863da9524c6")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'rde)
+        (url "file:///home/db/.local/share/git/rde")
+        (branch "master")
+        (commit
+          "497d2afcafa819dcda934723b6b6667427969446"))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "d1609e765bb48d1a5302ed0df4f1ed300a571eff")
+        (introduction
+          (make-channel-introduction
+            "46c1d8bcca674d3a71cd077c52dde9552a89873d"
+            (openpgp-fingerprint
+              "8141 6036 E81A 5CF7 8F80  1071 ECFC 8398 8B4E 4B9F"))))
+      (channel
+        (name 'emacs)
+        (url "https://github.com/babariviere/guix-emacs")
+        (branch "master")
+        (commit
+          "ada1cf8d3c3b932425750be824c343f97675c7e4")
+        (introduction
+          (make-channel-introduction
+            "72ca4ef5b572fea10a4589c37264fa35d4564783"
+            (openpgp-fingerprint
+              "261C A284 3452 FB01 F6DF  6CF4 F9B7 864F 2AB4 6F18")))))
